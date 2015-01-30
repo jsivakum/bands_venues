89252bf5b2950d1a2d52089b933267b8a7756dab
jsivakum <nayagan@gmail.com>
2015-01-30 10:41:42 -0800

    Add functionality to use proper database for rspec in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,15 @@
+ENV['RACK_ENV'] = 'test'
 require("bundler/setup")
 Bundler.require(:default, :test)
 Dir[File.dirname(__FILE__) + '/../lib/*.rb'].each {|file| require file}
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Band.all.each do |b|
+      b.destroy
+    end
+    Venue.all.each do |v|
+      v.destroy
+    end
+  end
+end

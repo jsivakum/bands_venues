b2f66af8b067fd6ce9e7b210480e972347098730
jsivakum <nayagan@gmail.com>
2015-01-30 11:01:26 -0800

    Add delete functionality to Bands

--- app.rb
+++ app.rb
@@ -4,19 +4,26 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each {|file| require file}
 also_reload("lib/**/*.rb")
 
 
-get('/') do
+get '/' do
   @bands = Band.all
 
   erb :index
 end
 
-post('/band') do
+post '/band' do
   name = params[:name]
   Band.create(:name => name)
   redirect back
 end
 
-get('/band/:id') do
+get '/band/:id' do
+  @band = Band.find(params[:id])
+  @venues = Venue.all
+  erb :venue
+end
 
-  erb: venue
+delete '/delete_band/:id' do
+  band_to_delete = Band.find(params[:id])
+  band_to_delete.destroy
+  redirect '/'
 end

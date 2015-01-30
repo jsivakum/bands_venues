78eda0bf3004e185df621fa7f91b6c4d05068b30
jsivakum <nayagan@gmail.com>
2015-01-30 13:28:16 -0800

    Add function to add venues in sinatra

--- app.rb
+++ app.rb
@@ -6,13 +6,19 @@ also_reload("lib/**/*.rb")
 
 get '/' do
   @bands = Band.all
-
+  @venues = Venue.all
   erb :index
 end
 
 post '/band' do
   name = params[:name]
   Band.create(:name => name)
+  redirect back
+end
+
+post '/venue' do
+  name = params[:name]
+  Venue.create(:name => name)
   redirect back
 end
 
@@ -31,5 +37,15 @@ end
 patch '/edit_band/:id' do
   band_to_edit = Band.find(params[:id])
   band_to_edit.update(:name => params[:name])
+  redirect back
+end
+
+patch '/venue/:id' do
+  @band = Band.find(params[:id])
+  @venue_ids = params[:venue_ids]
+  @venue_ids.each do |id|
+    v = Venue.find(id)
+    @band.venues << v
+  end
   redirect back
 end
